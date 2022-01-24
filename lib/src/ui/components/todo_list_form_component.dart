b1544e91cec88ef50a76f45b409f17d023be150b
Carlos Castro <carlos.carvalho@mjv.com.br>
2022-01-24 18:40:41 -0300

    created a component for avatar list

--- lib/src/ui/components/todo_list_form_component.dart
+++ lib/src/ui/components/todo_list_form_component.dart
@@ -97,8 +97,8 @@ class _ToDoListFormComponent extends State<ToDoListFormComponent> {
               height: 15,
             ),
 
-            // AvatarListWidget(),
-            listAvatar(),
+            AvatarListWidget(),
+            // listAvatar(),
             TextField(
               controller: _titleController,
               // decoration: InputDecoration(
@@ -228,89 +228,6 @@ class _ToDoListFormComponent extends State<ToDoListFormComponent> {
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  final _avatarList = [
-    CircleAvatar(
-        child: Icon(
-          FontAwesomeIcons.shoppingBasket,
-          color: AppColors.white,
-          size: 15,
-        ),
-        backgroundColor: AppColors.orange),
-    CircleAvatar(
-        child: Icon(
-          FontAwesomeIcons.basketballBall,
-          color: AppColors.white,
-          size: 15,
-        ),
-        backgroundColor: AppColors.pink),
-     CircleAvatar(
-        child: Icon(
-          Icons.location_on_outlined,
-          color: AppColors.white,
-          size: 15,
-        ),
-        backgroundColor: AppColors.blue),
-     CircleAvatar(
-        child: Icon(
-          FontAwesomeIcons.wineGlass,
-          color: AppColors.white,
-          size: 15,
-        ),
-        backgroundColor: AppColors.bluelight),
-   CircleAvatar(
-        child: Icon(
-          Icons.fitness_center_sharp,
-          color: AppColors.white,
-          size: 15,
-        ),
-        backgroundColor: AppColors.purple),
-     CircleAvatar(
-        child: Icon(
-          FontAwesomeIcons.bookReader,
-          color: AppColors.white,
-          size: 15,
-        ),
-        backgroundColor: AppColors.hexachrome_black),
-  ];
-
-  int selectIndex = 0;
-
-  listAvatar() {
-    return SizedBox(
-      height: 50,
-      width: MediaQuery.of(context).size.width,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: _avatarList.length,
-        itemBuilder: (_, index) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                boxShadow: selectIndex == index
-                    ? [
-                        BoxShadow(
-                            blurRadius: 0.8,
-                            color: AppColors.black,
-                            spreadRadius: 2)
-                      ]
-                    : null,
-              ),
-              child: GestureDetector(
-                child: (_avatarList[index]),
-                onTap: () {
-                  _selectedIcon = _avatarList[index];
-
-                  setState(() {
-                    selectIndex = index;
-                  });
-                },
-              ),
-            )),
       ),
     );
   }

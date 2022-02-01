5688ca36ecf5f4d211abd8a08381ad8a47c688d1
Dan0668079 <danielrodrigues452796@hotmail.com>
2022-02-01 11:59:17 -0300

    check login

--- lib/src/ui/pages/login_page.dart
+++ lib/src/ui/pages/login_page.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:teste/controllers/user_controller.dart';
-
 import 'signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -13,20 +15,19 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = "";
   String senha = "";
-
-  // Estados
   String error = "";
 
   late final userController = Provider.of<UserController>(
     context,
     listen: false,
   );
+  bool obscureTextPassword = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: const Text("Login"),
       ),
       body: Form(
         child: Padding(
@@ -34,49 +35,131 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Email'),
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.mail_outline,
+                      color: Color(0xffd17842),
+                    ),
+                    labelText: 'Email'),
                 onChanged: (texto) => email = texto,
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Senha'),
-                obscureText: true,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(
+                      Icons.enhanced_encryption_outlined,
+                      color: Color(0xffd17842),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          obscureTextPassword = !obscureTextPassword;
+                        });
+                      },
+                      icon: obscureTextPassword
+                          ? const Icon(Icons.visibility)
+                          : const Icon(Icons.visibility_off),
+                    ),
+                    labelText: 'Senha'),
+                obscureText: obscureTextPassword,
                 onChanged: (texto) => senha = texto,
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  try {
-                    await userController.login(email, senha);
-                  } on FirebaseAuthException catch (e) {
-                    var msg = "";
+              const SizedBox(height: 15),
+              Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      await userController.login(email, senha);
+                    } on FirebaseAuthException catch (e) {
+                      var msg = "";
 
-                    if (e.code == "wrong-password") {
-                      msg = "A senha está incorreta";
-                    } else if (e.code == "invalid-email") {
-                      msg = "Email inválido";
-                    } else {
-                      msg = "Ocorreu um erro";
+                      if (e.code == "wrong-password") {
+                        msg = "A senha está incorreta";
+                      } else if (e.code == "invalid-email") {
+                        msg = "Email inválido";
+                      } else if (e.code == "user-not-found") {
+                        msg = "Usuário não cadastrado";
+                      } else if (e.code == "too-many-requests") {
+                        msg = "Tente novamente mais tarde";
+                      } else if (e.code == "operation-not-allowed") {
+                        msg = "Login com email e senha não está habilitado";
+                      } else {
+                        msg = "Informe seu email e senha de cadastro";
+                      }
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(msg),
+                        ),
+                      );
                     }
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(msg),
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 30),
+                    child: const Text("Login"),
+                  ),
+                  style: ElevatedButton.styleFrom(primary: const Color(0xffd17842)),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignupPage(),
                       ),
                     );
-                  }
-                },
-                child: Text("Login"),
-              ),
-              OutlinedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SignupPage(),
+                  },
+                  child: Text(
+                    "Criar conta",
+                    style: GoogleFonts.blackOpsOne(
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        color: Color(0xFFFB6C34),
+                      ),
                     ),
-                  );
-                },
-                child: Text("Criar conta"),
-              )
+                  ),
+                ),
+              ),
+               Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child: OutlinedButton(
+                              onPressed: () async {
+                                try {
+                                  await userController.updateSenha(
+                                      email, context);
+                                } on FirebaseAuthException catch (e) {
+                                  var msg = "";
+                                  if (e.code == "invalid-email") {
+                                    msg = "Email inválido";
+                                  } else if (e.code == "user-not-found") {
+                                    msg = "Usuário não cadastrado";
+                                  } else {
+                                    msg = "Informe seu email de cadastro";
+                                    print(msg);
+                                  }
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(msg),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: Text(
+                                "Esqueci a senha",
+                                style: GoogleFonts.blackOpsOne(
+                                  textStyle: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xFFFB6C34),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ), 
+             
             ],
           ),
         ),
--- lib/src/ui/pages/signup_page.dart
+++ lib/src/ui/pages/signup_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:teste/controllers/user_controller.dart';
 import 'package:teste/src/data/model/user_model.dart';
 
+import 'login_page.dart';
 
 class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
   @override
   _SignupPageState createState() => _SignupPageState();
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
   String nome = "";
   String email = "";
   String senha = "";
@@ -18,43 +23,143 @@ class _SignupPageState extends State<SignupPage> {
     context,
     listen: false,
   );
+  bool obscureTextPassword = true;
+  bool obscureTextConfirmPassword = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Criar conta"),
+        title: const Text("Criar conta",),
       ),
-      body: Form(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Nome'),
-                onChanged: (texto) => nome = texto,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Email'),
-                onChanged: (texto) => email = texto,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Senha'),
-                obscureText: true,
-                onChanged: (texto) => senha = texto,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  final user = UserModel(nome: nome);
-                  await userController.signup(email, senha, user);
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 70),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Form(
+                      key: _formKey,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.person_add_alt,
+                                    color: Color(0xffd17842),
+                                  ),
+                                  labelText: 'Nome',),
+                              onChanged: (texto) => nome = texto,
+                              validator: (String? texto) {
+                                if (texto != null && texto.isNotEmpty) {
+                                  if (texto.length < 3) {
+                                    return "Digite um Nome válido.";
+                                  }
+                                } else {
+                                  return "Campo obrigatório.";
+                                }
+                              },
+                            ),
+                            TextFormField(
+                               validator: (String? texto) {
+                                if (texto != null && texto.isNotEmpty) {
+                                  if (!texto.contains('@') ||
+                                      texto.length < 8) {
+                                    return 'Digite um email válido';
+                                  }
+                                } else {
+                                  return 'Campo obrigatório.';
+                                }
+                              },
+                              onChanged: (texto) => email = texto,
+                              style: const TextStyle(color: Colors.black),
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.mail_outline,
+                                  color: Color(0xffd17842),
+                                ),
+                                labelText: 'Email',
+                              ),
+                            ),
+                            TextFormField(
+                           onChanged: (texto) => senha = texto,
+                              obscureText: obscureTextPassword,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.enhanced_encryption_outlined,
+                                  color: Color(0xffd17842),
+                                ),
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      obscureTextPassword =
+                                          !obscureTextPassword;
+                                    });
+                                  },
+                                  icon: obscureTextPassword
+                                      ? const Icon(Icons.visibility)
+                                      : const Icon(Icons.visibility_off),
+                                ),
+                                labelText: 'Senha',
+                              ),
+                              validator: (String? texto) {
+                                if (texto != null && texto.isNotEmpty) {
+                                  if (texto.length < 6) {
+                                    return "Digite uma senha com 6 caracteres ou mais";
+                                  }
+                                } else {
+                                  return "Campo obrigatório";
+                                }
+                              },
+                            ),
+                            ElevatedButton(
+                              onPressed: () async {
+                                final user = UserModel(nome: nome);
+                                await userController.signup(email, senha, user);
 
-                  Navigator.pop(context);
-                },
-                child: Text("Criar conta"),
-              )
-            ],
+                                Navigator.pop(context);
+                              },
+                              child: const Text("Criar conta"),
+                            ),
+                             Container(
+                              margin: EdgeInsets.only(top: 15),
+                              child: OutlinedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => LoginPage(),
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  "Tenho Cadastro",
+                                  style: GoogleFonts.blackOpsOne(
+                                    textStyle: TextStyle(
+                                      fontSize: 24,
+                                      color: Color(0xFFFB6C34),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
